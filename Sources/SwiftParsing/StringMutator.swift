7a5a9998be09273707876bfb27bef790ee4cbc96
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-10-11 04:01:03 +1000

    Added amount field for new lines.

--- Sources/SwiftParsing/StringMutator.swift
+++ Sources/SwiftParsing/StringMutator.swift
@@ -36,14 +36,18 @@ public struct StringMutator {
         return indentedLines.reduce("") { joinWithNewLines(str1: $0, str2: $1) }
     }
     
-    public func joinWithNewLines(str1: String, str2: String) -> String {
+    public func joinWithNewLines(str1: String, str2: String, amount: Int = 1) -> String {
+        guard amount > 0 else {
+            return str1 + str2
+        }
         if str1 == "" {
             return str2
         }
         if str2 == "" {
             return str1
         }
-        return str1 + "\n" + str2
+        let newLines = String(repeating: "\n", count: amount)
+        return str1 + newLines + str2
     }
     
 }
